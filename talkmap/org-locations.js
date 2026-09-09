a30c9e51cd01396a4579164be0ad60151d0e28d2
HaediThelen <haedi.thelen@pennmedicine.upenn.edu>
2026-09-09 17:18:04 -0400

    Update talk map.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Milan, Italy",
+    45.4641943,
+    9.1896346
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Berlin, Germany",
+    52.5173885,
+    13.3951309
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Boston, Massachusetts",
+    42.3588336,
+    -71.0578303
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Philadelphia, Pennsylvania",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Orlando, Florida",
+    28.5421218,
+    -81.379045
+  ],
+  [
+    "Washington, D.C.",
+    38.8950982,
+    -77.0363849
+  ],
+  [
+    "Phoenix, Arizona",
+    33.4484367,
+    -112.074141
   ]
 ];
